d16a366d91aa9b1842f13627b575ec92987416dd
Adelin Rosca <rosca.adelin@gmail.com>
2025-02-17 15:08:46 +0200

    intalled the dependencies

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+//file created automatically by shadcn
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
