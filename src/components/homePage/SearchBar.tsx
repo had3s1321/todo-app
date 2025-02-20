66a2fe2fee85b2f278079599d22b8d202dd2a1ca
Adelin Rosca <rosca.adelin@gmail.com>
2025-02-20 13:43:56 +0200

    added icons and functionality to the search bar

--- src/components/homePage/SearchBar.tsx
+++ src/components/homePage/SearchBar.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { ArrowRightIcon, SearchIcon } from "../ui/icons";
 import { Input } from "../ui/input";
 
 const SearchBar = () => {
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
   return (
-    <div>
-      <Input searchBar placeholder="Search..." />
+    <div className="relative w-full [&>svg]:absolute [&>svg]:left-0 [&>svg]:top-0 [&>svg]:ml-3 [&>svg]:mt-[20px]">
+      <Input
+        searchBar
+        placeholder="Search..."
+        value={value}
+        onChange={handleChange}
+      />
+      <SearchIcon />
+      <Button
+        variant="lgCustom"
+        size="lgCustom"
+        className="absolute right-0 top-0 mr-3 mt-3"
+      >
+        <ArrowRightIcon />
+      </Button>
     </div>
   );
 };
