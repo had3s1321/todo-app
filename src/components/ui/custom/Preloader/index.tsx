b82c80685a9e830ef3c100d26d6b833e7b16b524
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-11 15:15:51 +0200

    added preloader component

---
+++ src/components/ui/custom/Preloader/index.tsx
@@ -0,0 +1,11 @@
+import { BouncingCircles } from "./BouncingCircles";
+
+const Preloader = () => {
+  return (
+    <div className="flex h-dvh w-dvw items-center justify-center">
+      <BouncingCircles />
+    </div>
+  );
+};
+
+export default Preloader;
